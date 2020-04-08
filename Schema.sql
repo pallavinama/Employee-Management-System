2957240defc5ba2349166a34560159625ecde052
pallavi <pallavi_nama@yahoo.com>
2020-04-08 00:07:31 -0400

    rearraning

--- Schema.sql
+++ Schema.sql
@@ -6,23 +6,26 @@ USE ems_DB;
 
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
--- CREATE TABLE role (
---   id INT NOT NULL AUTO_INCREMENT,
---   title VARCHAR(30) NOT NULL,
---   salary DECIMAL(10,2),
---   department_id FORIEGN KEY,
---   PRIMARY KEY (id)
--- );
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary decimal not null,
+  department_id int,
+  PRIMARY KEY (id),
+  foreign key (department_id) references department(id) on delete cascade  
+);
 
--- CREATE TABLE employee (
---   id INT NOT NULL AUTO_INCREMENT,
---   first_name VARCHAR(30),
---   last_name VARCHAR(30),
---   role_id Foreign Key,
---   manager_id INT,
---   PRIMARY KEY (id)
--- );
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name varchar(30) not null,
+  role_id int,
+  manager_id int,
+  PRIMARY KEY (id),
+  foreign key (role_id) references role(id) on delete cascade  
+);
+
